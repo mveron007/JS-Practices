14b6ccbd7e34fb2f9d1724fdb699e5158218d930
mveron007 <mveron13814@gmail.com>
2021-07-18 23:27:08 -0300

    Adding Index Page for redirect to each project

--- proyecto-citas/js/app.js
+++ proyecto-citas/js/app.js
@@ -7,8 +7,71 @@ const horarioInput = document.querySelector("#horario");
 const sintomasInput = document.querySelector("#sintomas");
 
 const form = document.querySelector("#formulario");
-const citas = document.querySelector("#citas");
+const listaCitas = document.querySelector("#citas");
+const btnCita = document.querySelector("#btnCita");
 
+class Cita{
+    constructor(){
+        this.citas = [];
+    }
+
+    agregarCita(cita){
+        this.citas = [...this.citas, cita];
+    }
+
+}
+
+class UI{
+
+    imprimirAlerta(mensaje, tipo) {
+        // Crear div mensaje
+        const divMensaje = document.createElement("div");
+        divMensaje.classList.add("text-center", "d-block", "alert", "col-12");
+
+        //Clase segun tipo
+        if (tipo === "error") {
+            divMensaje.classList.add("alert-danger");
+        }else{
+            divMensaje.classList.add("alert-success");
+        }
+
+        divMensaje.textContent = mensaje;
+
+        // Agregar al DOM
+        document.querySelector(".row").insertBefore(divMensaje, document.querySelector("#datos-paciente"));
+
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 3000);
+    }
+
+
+    imprimirCitas({citas}){
+        console.log(citas);
+        citas.forEach(cita => {
+            const {mascota, propietario, telefono, fecha, horario, sintomas, id} = cita;
+
+            const divCita = document.createElement("div");
+            // divCita.classList.add("p-3");
+            divCita.classList.add("card");
+            divCita.dataset.id = id;
+            const titleMascota = document.createElement("h5");
+            titleMascota.classList.add("card-title");
+            titleMascota.textContent = mascota;
+
+            divCita.appendChild(titleMascota);
+
+            listaCitas.appendChild(divCita);
+
+
+        });
+    }
+}
+
+
+const ui = new UI();
+
+const adminCitas = new Cita();
 ///// EVENTOS
 eventListeners();
 
@@ -21,6 +84,8 @@ function eventListeners(){
     fechaInput.addEventListener("input", datosCita);
     horarioInput.addEventListener("input", datosCita);
     sintomasInput.addEventListener("input", datosCita);
+
+    form.addEventListener("submit", nuevaCita);
 
 }
 
@@ -35,5 +100,38 @@ const citaObj = {
 
 function datosCita(e){
     citaObj[e.target.name] = e.target.value;
+}
+
+function nuevaCita(e){
+    e.preventDefault();
+
+    const {mascota, propietario, telefono, fecha, horario, sintomas} = citaObj;
     console.log(citaObj);
+    if (mascota === "" || propietario === "" || telefono === "" || fecha === "" || horario === "" || sintomas === "") {
+        ui.imprimirAlerta("Todos los campos son obligatorios", "error");
+        return;
+    }
+
+    //Id Unico
+    citaObj.id = Date.now();
+    console.log(citaObj);
+    adminCitas.agregarCita({...citaObj});
+
+    //Reiniciar Objeto
+    reiniciarObj();
+
+    form.reset();
+
+    ui.imprimirCitas(adminCitas);
+}
+
+
+function reiniciarObj() {
+    citaObj.mascota = "";
+    citaObj.propietario = "";
+    citaObj.telefono = "";
+    citaObj.fecha = "";
+    citaObj.horario = "";
+    citaObj.sintomas = "";
+
 }
